ad6b2fe6d6efef1da4e4ec370d23caaba4459a67
Matt Johnson <mj1856@hotmail.com>
2016-02-11 00:02:27 -0600

    Bump version to 1.1.0

--- NodaTime.NetworkClock/Properties/AssemblyInfo.cs
+++ NodaTime.NetworkClock/Properties/AssemblyInfo.cs
@@ -2,9 +2,9 @@
 
 [assembly: AssemblyCompany("Matt Johnson")]
 [assembly: AssemblyProduct("NodaTime.NetworkClock")]
-[assembly: AssemblyCopyright("Copyright © 2014, Matt Johnson.  MIT Licensed.")]
+[assembly: AssemblyCopyright("Copyright © Matt Johnson.  MIT Licensed.")]
 [assembly: AssemblyTitle("NodaTime.NetworkClock")]
 [assembly: AssemblyDescription("A NodaTime.IClock implementation that gets the current time from an NTP server instead of the computer's local clock.")]
 
-[assembly: AssemblyVersion("1.0.1.*")]
-[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyVersion("1.1.0.*")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
